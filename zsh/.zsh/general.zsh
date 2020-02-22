16c0a7ca058b43b90d6e55cd2959329285440bc0
Glenn Hope <glenn.alexander.hope@gmail.com>
2020-02-21 18:42:47 -0800

    Add zsh configs

---
+++ zsh/.zsh/general.zsh
@@ -0,0 +1,81 @@
+#
+# .zshrc is sourced in interactive shells.
+# It should contain commands to set up aliases,
+# functions, options, key bindings, etc.
+#
+
+export EDITOR="vim"
+export PATH="$PATH:/home/glennh/bin:/home/glennh/bin/sandcastle"
+
+export ADMIN_SCRIPTS=/mnt/vol/engshare/admin/scripts
+[ -f "${ADMIN_SCRIPTS}/scm-prompt" ] && source "${ADMIN_SCRIPTS}/scm-prompt"
+
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=10000
+HISTORY_IGNORE="(ls|cd|pwd|exit|cd|k|j)"
+SAVEHIST=10000
+setopt appendhistory
+setopt APPEND_HISTORY
+setopt INC_APPEND_HISTORY
+#setopt SHARE_HISTORY
+
+export CDPATH=~
+
+## never ever beep ever
+setopt NO_BEEP
+unsetopt beep
+
+# vim mode
+bindkey -v
+export KEYTIMEOUT=1
+
+autoload -Uz compinit
+compinit
+
+#allow tab completion in the middle of a word
+#setopt COMPLETE_IN_WORD
+
+## keep background processes at full speed
+setopt NOBGNICE
+
+## automatically decide when to page a list of completions
+LISTMAX=0
+
+## disable mail checking
+#MAILCHECK=0
+
+autoload -U colors
+colors
+
+setopt PROMPT_SUBST
+#export PROMPT='%F{yellow}[%F{cyan}%D{%a %y/%m/%d %R %Z}%F{yellow}]%F{yellow}[%F{green}%l%F{yellow}]$([[ `_dotfiles_scm_info` == "" ]] || echo "%F{yellow}[%F{cyan}$(_dotfiles_scm_info | sed "s/^ //g" | tr -d "()")%F{yellow}]")%(?..[%F{red}%?%1v%F{yellow}] )
+#%F{yellow}<%F{green}%n@%m%F{white}:%F{yellow}%~%F{yellow}>
+#%f%B$%b '
+
+export PROMPT='%F{yellow}[%F{cyan}%D{%a %y/%m/%d %R %Z}%F{yellow}]%F{yellow}[%F{green}%l%F{yellow}]%(?..[%F{red}%?%1v%F{yellow}] )
+%F{yellow}<%F{green}%n@%m%F{white}:%F{yellow}%~%F{yellow}>
+%f%B$%b '
+
+#export PROMPT='%F{yellow}[%F{cyan}%D{%a %y/%m/%d %R %Z}%F{yellow}]%F{yellow}[%F{green}%l%F{yellow}]%F{yellow}])%(?..[%F{red}%?%1v%F{yellow}] )
+#%F{yellow}<%F{green}%n@%m%F{white}:%F{yellow}%~%F{yellow}>
+#%f%B$%b '
+
+# Real comments
+setopt interactivecomments
+
+alias ls='ls --color=auto'
+
+# Anti-TMUX Alliance
+for FBSOURCE_TYPE in fbcode fbandroid fbobjc; do
+    prefix="^/data/users/$USER/fbsource/$FBSOURCE_TYPE"
+    if grep -q "$prefix" <<< "$PWD"; then 
+        suffix=$(sed "s,$prefix,," <<< "$PWD")
+        cd ~/$FBSOURCE_TYPE/$suffix
+    fi
+done
+prefix="^/data/users/$USER/opsfiles-hg"
+if grep -q "$prefix" <<< "$PWD"; then 
+    suffix=$(sed "s,$prefix,," <<< "$PWD")
+    cd ~/opsfiles/$suffix
+fi
---
+++ zsh/.zsh/viins.zsh
@@ -0,0 +1,26 @@
+bindkey -M viins "\e." insert-last-word
+bindkey -M viins "^?" backward-delete-char
+bindkey -M viins "^[[A" history-beginning-search-backward
+bindkey -M viins "^[[B" history-beginning-search-forward
+bindkey -M viins "^L" clear-screen
+bindkey -M viins "^A" beginning-of-line
+bindkey -M viins "^E" end-of-line
+bindkey -M viins "^W" backward-kill-word
+bindkey -M viins "^U" backward-kill-line
+bindkey -M viins "^K" vi-kill-eol
+bindkey -M viins "^R" history-incremental-search-backward
+
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
+
+autoload -Uz copy-earlier-word
+zle -N copy-earlier-word
+bindkey "^[m" copy-earlier-word
+
+real-paste() {
+    zle vi-put-after
+    zle vi-forward-char
+}
+zle -N real-paste
+bindkey -M viins "^Y" real-paste
