b0fb9b97cbb4e3326e83ee062fda7e316815ecd2
Glenn Hope <glenn.alexander.hope@gmail.com>
2023-01-31 12:34:21 -0800

    Update run script since stow --dotfiles doesn't work

--- run.sh
+++ run.sh
@@ -1,10 +1,24 @@
-#!/bin/bash -x
+#!/bin/bash
 
-progs=$(cat active_progs.txt)
+thisdir="$(dirname "$0")"
+cd "$thisdir"
 
-which stow >/dev/null || ( echo 'GNU Stow is not installed. Try `sudo pacman -S stow` or `sudo apt install stow`.' && exit 1 )
+#progs=$(cat active_progs.txt)
 
-for prog in $progs; do
-    echo "Populating dotfiles from '$prog'..."
-    stow --dotfiles $prog
-done
+# See .gitignore for ignored files
+#ignore_regex='(\.vim/|\.zsh/\.zcompdump)'
+
+#which stow >/dev/null || ( echo 'GNU Stow is not installed. Try `sudo pacman -S stow` or `sudo apt install stow`.' && exit 1 )
+
+#for prog in $progs; do
+    #echo "Populating dotfiles from '$prog'..."
+    #stow -v --dotfiles $prog --ignore="${ignore_regex}"
+#done
+
+# Unfortunately, --dotfiles is broken for directories with stow
+ln -sf "$(pwd)"/vim/dot-vimrc ~/.vimrc
+ln -sf "$(pwd)"/vim/dot-vim ~/.vim
+ln -sf "$(pwd)"/zsh/dot-zshrc ~/.zshrc
+ln -sf "$(pwd)"/zsh/dot-zsh ~/.zsh
+#ln -sf tmux.conf ~/.tmux.conf
+ln -sf "$(pwd)"/shell_aliases/dot-glennh_aliases.sh ~/.glennh_aliases.sh
