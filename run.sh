ddb2509dfa6cded2d9e8c43154be4b7977e535d4
Glenn Hope <glenn.alexander.hope@gmail.com>
2023-01-31 15:45:53 -0800

    Better symlinking

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO: find a replacement for stow
+# TODO: pull in zsh changes from Dropbox
+
 thisdir="$(dirname "$0")"
 cd "$thisdir"
 
@@ -15,10 +18,22 @@ cd "$thisdir"
     #stow -v --dotfiles $prog --ignore="${ignore_regex}"
 #done
 
-# Unfortunately, --dotfiles is broken for directories with stow
-ln -sf "$(pwd)"/vim/dot-vimrc ~/.vimrc
-ln -sf "$(pwd)"/vim/dot-vim ~/.vim
-ln -sf "$(pwd)"/zsh/dot-zshrc ~/.zshrc
-ln -sf "$(pwd)"/zsh/dot-zsh ~/.zsh
-#ln -sf tmux.conf ~/.tmux.conf
-ln -sf "$(pwd)"/shell_aliases/dot-glennh_aliases.sh ~/.glennh_aliases.sh
+# Unfortunately, --dotfiles is broken for directories with stow, so do this by hand until you find a suitable replacement
+for dotfiles_filename_local in vim/* \
+	zsh/* \
+	shell_aliases/*
+do
+	dotfiles_filename_full="$(pwd)/${dotfiles_filename_local}"
+
+	target_filename=$(echo "$dotfiles_filename_local" | sed -E 's,[^/]+/dot-([^/]+)$,.\1,')
+	target_filename_full="${HOME}/${target_filename}"
+
+
+	if [ -L "$target_filename_full" ] || [ ! -e "$target_filename_full" ]; then
+		echo "[INFO] Linking '$target_filename_full' -> '$dotfiles_filename_full'"
+		ln -snf "$dotfiles_filename_full" "$target_filename_full"
+	else
+		echo "[ERROR] '$target_filename_full' exists and is not a symlink, skipping!"
+	fi
+
+done
