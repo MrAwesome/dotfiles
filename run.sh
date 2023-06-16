617b09abbc1930d338a9ab2d7db8373de88196ca
MrAwesome <glenn.alexander.hope@gmail.com>
2023-06-16 15:48:23 -0700

    Major update to per-hostname dotfile handling

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@
 # TODO: find a replacement for stow
 # TODO: pull in zsh changes from Dropbox
 
+set -euo pipefail
+
 # set to the absolute location of this script
 thisdir="$(cd "$(dirname "$0")" && pwd)"
+cd "$thisdir"
 
 #progs=$(cat active_progs.txt)
 
@@ -14,27 +17,49 @@ thisdir="$(cd "$(dirname "$0")" && pwd)"
 #which stow >/dev/null || ( echo 'GNU Stow is not installed. Try `sudo pacman -S stow` or `sudo apt install stow`.' && exit 1 )
 
 #for prog in $progs; do
-    #echo "Populating dotfiles from '$prog'..."
-    #stow -v --dotfiles $prog --ignore="${ignore_regex}"
+#echo "Populating dotfiles from '$prog'..."
+#stow -v --dotfiles $prog --ignore="${ignore_regex}"
 #done
 
 # Unfortunately, --dotfiles is broken for directories with stow, so do this by hand until you find a suitable replacement
-for dotfiles_filename_local in vim/* \
-	zsh/* \
-	shell_aliases/*
+for filename_local in vim/* \
+    xinitrc/* \
+    Xdefaults/* \
+    zsh/* \
+    shell_aliases/*
 do
-	dotfiles_filename_full="$(pwd)/${dotfiles_filename_local}"
+    echo "[$filename_local]"
 
-	target_filename=$(echo "$dotfiles_filename_local" | sed -E 's,[^/]+/dot-([^/]+)$,.\1,')
-	target_filename_full="${HOME}/${target_filename}"
+    final_linkname_no_dot=$(echo "$filename_local" | sed -E 's,[^/]+/dot-([^/]+)$,\1,')
+    # if the filename ends in -hostname:<hostname>, then only link it if the hostname matches
+    has_hostname=$(echo "$filename_local" | grep -E '/dot-[^/]*?-hostname:[^/]+$' || true)
+    if [[ "$has_hostname" != "" ]]; then
+        desired_hostname=$(echo "$filename_local" | sed -E 's,[^/]+/dot-[^/]+-hostname:([^/]+)$,\1,')
+        current_hostname=$(cat /etc/hostname)
+        if [[ "$desired_hostname" != "$current_hostname" ]]; then
+            echo "(SKIP: hostname mismatch)"
+            echo
+            #echo "[INFO] Skipping '$target_filename_full' because hostname '$hostname' does not match '${hostname}'"
+            continue
+        else
+            final_linkname_no_dot=$(echo "$filename_local" | sed -E 's,[^/]+/dot-([^/]+)(-hostname:[^/]+)$,\1,')
+        fi
 
+    fi
 
-	if [ -L "$target_filename_full" ] || [ ! -e "$target_filename_full" ]; then
-		echo "[INFO] Linking '$target_filename_full' -> '$dotfiles_filename_full'"
-		ln -snf "$dotfiles_filename_full" "$target_filename_full"
-	else
-		echo "[ERROR] '$target_filename_full' exists and is not a symlink, skipping!"
-	fi
+    target_filename_full="${thisdir}/${filename_local}"
+
+    linkname_full="${HOME}/.${final_linkname_no_dot}"
+
+    if [ -L "$linkname_full" ] || [ ! -e "$linkname_full" ]; then
+        echo "$linkname_full ->"
+        echo "   $target_filename_full"
+        ln -snf "$target_filename_full" "$linkname_full"
+        echo
+    else
+        echo "[ERROR] '$linkname_full' exists and is not a symlink, skipping!"
+        echo
+    fi
 done
 
 mkdir -p ~/bin
