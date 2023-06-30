5fa8801b2f8134cc23100bbc5adfeb6ec695d4cf
MrAwesome <glenn.alexander.hope@gmail.com>
2023-06-30 14:46:07 -0700

    Fix attachment to tmux session

--- bin/weechat_tmux.sh
+++ bin/weechat_tmux.sh
@@ -4,7 +4,10 @@ REM_HOST="gleesus.net"
 
 # TODO: check for .weechat_master instead, and set that from ansible
 if [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
-    tmux attach-session -t weechat || tmux new-session -s weechat -d weechat
+    tmux attach-session -t weechat || \
+        # start and attach to weechat session:
+        (tmux new-session -s weechat -d weechat && \
+            tmux attach-session -t weechat)
 else
     ssh -t "$REM_HOST" "./bin/weechat_tmux.sh"
 fi
