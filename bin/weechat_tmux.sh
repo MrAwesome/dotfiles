9f9c678e0aec4bb66dc49b65897ad27cc2d9aa30
MrAwesome <glenn.alexander.hope@gmail.com>
2023-06-30 17:05:55 -0700

    Use built-in tmux attaching

--- bin/weechat_tmux.sh
+++ bin/weechat_tmux.sh
@@ -2,12 +2,10 @@
 
 REM_HOST="gleesus.net"
 
+# TODO: if weechat master, start weechat service in tmux instead of waiting for connect to start it?
 # TODO: check for .weechat_master instead, and set that from ansible
 if [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
-    tmux attach-session -t weechat || \
-        # start and attach to weechat session:
-        (tmux new-session -s weechat -d weechat && \
-            tmux attach-session -t weechat)
+    tmux new-session -A -s weechat weechat
 else
     ssh -t "$REM_HOST" "./bin/weechat_tmux.sh"
 fi
